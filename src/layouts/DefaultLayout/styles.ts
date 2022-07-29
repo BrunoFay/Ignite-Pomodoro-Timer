a3a118961de580bdcfc732291ab711bc8125eebb
FAY <brunofay1@hotmail.com>
2022-07-28 21:30:57 -0300

    NEW implement visual history page

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
+  box-sizing: border-box;
   max-width: 74rem;
-  height: calc(100vh - 10rem);
+  height: calc(100% - 10rem);
   margin: 5rem auto;
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
   display: flex;
-  padding: 2rem;
+  padding: 2.5rem;
   flex-direction: column;
 `
